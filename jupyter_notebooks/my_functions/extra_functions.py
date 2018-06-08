2c56e5da5d6bb729eb5d70c1f2873fdc33b55015
Alijah O'Connor <alijah.oconnor@yahoo.com>
2018-06-08 11:55:23 -0400

    Changed A lot - Structure of [Flag, DE/AP Only], added Filtered Out School Breakdown in 04, Changed the Enrollment File to use the filtered dataset (and add Locale Breakdowns)

--- jupyter_notebooks/my_functions/extra_functions.py
+++ jupyter_notebooks/my_functions/extra_functions.py
@@ -1,3 +1,21 @@
+import pandas as pd
+
+"""  Used with a mapping fuction to create a locale column on a DataFrame  """
+locale_map = {11: 'City', 12: 'City', 13: 'City', 14: 'City',
+              21: 'Suburban', 22: 'Suburban', 23: 'Suburban', 24: 'Suburban',
+              31: 'Town', 32: 'Town', 33: 'Town', 34: 'Town',
+              41: 'Rural', 42: 'Rural', 43: 'Rural', 44: 'Rural'}
+
+def students_in_11_or_12(g11, g12):
+    """Inputs 11th and 12th grade enrollment flags from the CRDC dataset
+        Returns a label representing whether or not a school has students in 11th or 12th grade
+        """
+    if g11 == 'Yes':
+        return 'Yes'
+    if g12 == 'Yes':
+        return 'Yes'
+    return 'No'
+
 def hs_enrollment_averager(df):
     """Provides a work-around for figuring out the number of high school students in a school that offers grades 8th
         or lower.  Scales the total enrollment of each school by the ratio of high school grades offered : total grades
@@ -109,3 +127,67 @@ def eth_grouper(pct):
     elif .6 < pct <= .8:
         return 4
     return 5
+
+def de_ap_enrollments(de_only, ap_only, neither, both, groupby_field=False):
+    """ Takes in four DataFrames (de_only, ap_only, neither, and both) and
+        breaks their high-school (calculated by the hs_enrollment averager function) enrollments
+        down by a specific field (if specified) """
+
+    if groupby_field != False:
+        de_only_field = de_only.groupby(groupby_field)['hs_total_enrollment'].sum().rename('DE_Only_hs_students')
+        ap_only_field = ap_only.groupby(groupby_field)['hs_total_enrollment'].sum().rename('AP_Only_hs_students')
+        neither_AP_DE_field = neither.groupby(groupby_field)['hs_total_enrollment'].sum().rename('Neither_AP_DE_hs_students')
+        both_AP_DE_field = both.groupby(groupby_field)['hs_total_enrollment'].sum().rename('Both_AP_DE_hs_students')
+
+        combined = pd.concat([de_only_field, ap_only_field,
+                              neither_AP_DE_field, both_AP_DE_field], axis = 1).fillna(0).astype(int)
+
+        combined['Total'] = combined['DE_Only_hs_students'] + combined['AP_Only_hs_students'] \
+                            + combined['Neither_AP_DE_hs_students'] + combined['Both_AP_DE_hs_students']
+
+        combined['DE_Only %'] = round(combined['DE_Only_hs_students'] / combined['Total'] * 100, 1)
+        combined['AP_Only %'] = round(combined['AP_Only_hs_students'] / combined['Total'] * 100, 1)
+        combined['Neither_AP_DE %'] = round(combined['Neither_AP_DE_hs_students'] / combined['Total'] * 100, 1)
+        combined['Both_AP_DE %'] = round(combined['Both_AP_DE_hs_students'] / combined['Total'] * 100, 1)
+
+        order = ['DE_Only_hs_students', 'DE_Only %', 'AP_Only_hs_students', 'AP_Only %',
+                 'Neither_AP_DE_hs_students', 'Neither_AP_DE %', 'Both_AP_DE_hs_students', 'Both_AP_DE %', 'Total']
+
+        return combined[order]
+    else:
+        de_only_field = de_only['hs_total_enrollment'].sum()#.rename('DE_Only_hs_students')
+        ap_only_field = ap_only['hs_total_enrollment'].sum()#.rename('AP_Only_hs_students')
+        neither_AP_DE_field = neither['hs_total_enrollment'].sum()#.rename('Neither_AP_DE_hs_students')
+        both_AP_DE_field = both['hs_total_enrollment'].sum()#.rename('Both_AP_DE_hs_students')
+
+        combined = pd.DataFrame({'DE_Only_hs_students': de_only_field,
+                                 'AP_Only_hs_students': ap_only_field,
+                                 'Neither_AP_DE_hs_students': neither_AP_DE_field,
+                                 'Both_AP_DE_hs_students': both_AP_DE_field},
+                               index = ['HS Enrollments'])
+
+        combined['Total'] = combined['DE_Only_hs_students'] + combined['AP_Only_hs_students'] \
+                            + combined['Neither_AP_DE_hs_students'] + combined['Both_AP_DE_hs_students']
+
+        combined['DE_Only %'] = round(combined['DE_Only_hs_students'] / combined['Total'] * 100, 1)
+        combined['AP_Only %'] = round(combined['AP_Only_hs_students'] / combined['Total'] * 100, 1)
+        combined['Neither_AP_DE %'] = round(combined['Neither_AP_DE_hs_students'] / combined['Total'] * 100, 1)
+        combined['Both_AP_DE %'] = round(combined['Both_AP_DE_hs_students'] / combined['Total'] * 100, 1)
+
+        order = ['DE_Only_hs_students', 'DE_Only %', 'AP_Only_hs_students', 'AP_Only %',
+                 'Neither_AP_DE_hs_students', 'Neither_AP_DE %', 'Both_AP_DE_hs_students', 'Both_AP_DE %', 'Total']
+
+        return combined[order]
+
+def add_de_ap_pcts(df_input):
+    """ Adds a percentage breakdown of all the columns (DE_Only, AP_Only, etc.) in the inputted DataFrame (df) """
+    df = df_input.copy()
+    df['DE_Only %'] = round(df['DE_Only'] / df['Total'] * 100, 1)
+    df['AP_Only %'] = round(df['AP_Only'] / df['Total'] * 100, 1)
+    df['Neither_AP_DE %'] = round(df['Neither_AP_DE'] / df['Total'] * 100, 1)
+    df['Both_AP_DE %'] = round(df['Both_AP_DE'] / df['Total'] * 100, 1)
+
+    order = ['DE_Only', 'DE_Only %', 'AP_Only', 'AP_Only %',
+             'Neither_AP_DE', 'Neither_AP_DE %', 'Both_AP_DE', 'Both_AP_DE %', 'Total']
+
+    return df[order]
